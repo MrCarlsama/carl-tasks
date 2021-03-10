ed50aedb73d3357dba7b54a63649ca80340b28f3
carlsama <13531292590>
2021-03-10 13:44:58 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   useEffect(() => {
-    nothing()
-  })
+    nothing();
+  });
 
   const nothing = () => {
     let a = 2;
-  }
+  };
 
   return (
     <div className="App">
-      <header className="App-header"><img
-          src={logo} className="App-logo" alt="logo" /><p> Edit <code>src/App.tsx</code> and save to reload.
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {" "}
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link"
-          href="https://reactjs.org" target="_blank"
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
           rel="noopener noreferrer"
-        >Learn React
+        >
+          Learn React
         </a>
       </header>
     </div>
