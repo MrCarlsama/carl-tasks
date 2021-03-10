ba26a43b6f4c993483fe1610189ea093df3990b9
carlsama <13531292590>
2021-03-10 13:14:23 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /><p> Edit <code>src/App.tsx</code> and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+        <img src={logo} className="App-logo" alt="logo" /><p> Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link"
+          href="https://reactjs.org" target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        >Learn React
         </a>
       </header>
     </div>
