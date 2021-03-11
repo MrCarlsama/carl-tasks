9ad7068e85d17b750caf4afefd9fbebb480e037e
carlsama <13531292590>
2021-03-11 13:51:16 +0800

    feat: add json-server middleware login-views

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import ProjectListScreen from "screens/project-list";
+import LoginScreen from "login";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      {/* <ProjectListScreen /> */}
+      <LoginScreen />
     </div>
   );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value: any) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
 export const cleanObject = (object: object) => {
   const result = { ...object };
@@ -21,7 +21,7 @@ export const useMount = (callback: () => void) => {
   }, []);
 };
 
-export const useDebounce = (value: any, delay?: number) => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debouncedValue, setDebounceValue] = useState(value);
 
   useEffect(() => {
@@ -33,4 +33,19 @@ export const useDebounce = (value: any, delay?: number) => {
   }, [value, delay]);
 
   return debouncedValue;
+};
+
+export const useArray = <T>(initialArray: T[]) => {
+  const [value, setValue] = useState(initialArray);
+  return {
+    value,
+    setValue,
+    add: (item: T) => setValue([...value, item]),
+    clear: () => setValue([]),
+    removeIndex: (index: number) => {
+      const copy = [...value];
+      copy.splice(index, 1);
+      setValue(copy);
+    },
+  };
 };
