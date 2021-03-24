1131b18a041f4708ec8bfb1dc77554394204a577
carlsama <13531292590>
2021-03-24 14:39:37 +0800

    feat: modify useAsync by useReducer

--- src/utils/useAsync.ts
+++ src/utils/useAsync.ts
@@ -1,5 +1,5 @@
 import { useMountedRef } from "./index";
-import { useCallback, useState } from "react";
+import { useCallback, useReducer, useState } from "react";
 interface State<D> {
   error: Error | null;
   data: D | null;
@@ -16,38 +16,53 @@ const defaultConfig = {
   throwOnError: false,
 };
 
+const useSafeDispatch = <T>(dispatch: (...args: T[]) => void) => {
+  const mountedRef = useMountedRef();
+  return useCallback(
+    (...args: T[]) => (mountedRef.current ? dispatch(...args) : void 0),
+    [dispatch, mountedRef]
+  );
+};
+
 export const useAsync = <D>(
   initialState?: State<D>,
   initialConfig?: typeof defaultConfig
 ) => {
   const config = { ...defaultConfig, ...initialConfig };
-  const [state, setState] = useState<State<D>>({
-    ...defaultInitialState,
-    ...initialState,
-  });
-
-  const mountedRef = useMountedRef();
+  // const [state, setState] = useState<State<D>>({
+  //   ...defaultInitialState,
+  //   ...initialState,
+  // });
+  const [state, dispatch] = useReducer(
+    (state: State<D>, action: Partial<State<D>>) => ({ ...state, ...action }),
+    {
+      ...defaultInitialState,
+      ...initialState,
+    }
+  );
+  // const mountedRef = useMountedRef();
+  const safeDispatch = useSafeDispatch(dispatch);
 
   const [retry, setRetry] = useState(() => () => {});
 
   const setData = useCallback(
     (data: D) =>
-      setState({
+      safeDispatch({
         data,
         stat: "success",
         error: null,
       }),
-    []
+    [safeDispatch]
   );
 
   const setError = useCallback(
     (error: Error) =>
-      setState({
+      safeDispatch({
         error,
         stat: "error",
         data: null,
       }),
-    []
+    [safeDispatch]
   );
 
   const run = useCallback(
@@ -58,11 +73,12 @@ export const useAsync = <D>(
       setRetry(() => () => {
         if (runConfig?.retry) run(runConfig?.retry(), runConfig);
       });
-      setState((prevState) => ({ ...prevState, stat: "loading" }));
+      // dispatch((prevState) => ({ ...prevState, stat: "loading" }));
+      safeDispatch({ stat: "loading" });
 
       return promise
         .then((data) => {
-          if (mountedRef.current) setData(data);
+          setData(data);
           return data;
         })
         .catch((error) => {
@@ -71,7 +87,7 @@ export const useAsync = <D>(
           return error;
         });
     },
-    [config.throwOnError, mountedRef, setData, setError]
+    [config.throwOnError, safeDispatch, setData, setError]
   );
 
   return {
--- src/utils/useUndo.ts
+++ src/utils/useUndo.ts
@@ -1,20 +1,28 @@
-import { useCallback, useState } from "react";
-export const useUndo = <T>(initialPresent: T) => {
-  const [state, setState] = useState<{
-    past: T[];
-    present: T;
-    future: T[];
-  }>({
-    past: [],
-    present: initialPresent,
-    future: [],
-  });
+import { useCallback, useReducer } from "react";
 
-  const undo = useCallback(() => {
-    setState((currentState) => {
-      const { past, present, future } = currentState;
+const UNDO = "UNDO";
+const REDO = "REDO";
+const SET = "SET";
+const RESET = "RESET";
 
-      if (past.length === 0) return currentState;
+type State<T> = {
+  past: T[];
+  present: T;
+  future: T[];
+};
+
+type Action<T> = {
+  newPresent?: T;
+  type: typeof UNDO | typeof REDO | typeof SET | typeof RESET;
+};
+
+const undoReducer = <T>(state: State<T>, action: Action<T>) => {
+  const { past, present, future } = state;
+  const { newPresent, type } = action;
+
+  switch (type) {
+    case UNDO: {
+      if (past.length === 0) return state;
 
       const previous = past[past.length - 1];
       const newPast = past.slice(0, past.length - 1);
@@ -23,14 +31,10 @@ export const useUndo = <T>(initialPresent: T) => {
         present: previous,
         future: [present, ...future],
       };
-    });
-  }, []);
+    }
 
-  const redo = useCallback(() => {
-    setState((currentState) => {
-      const { past, present, future } = currentState;
-
-      if (future.length === 0) return currentState;
+    case REDO: {
+      if (future.length === 0) return state;
 
       const next = future[0];
       const newFuture = future.slice(1);
@@ -39,30 +43,94 @@ export const useUndo = <T>(initialPresent: T) => {
         present: next,
         future: newFuture,
       };
-    });
-  }, []);
+    }
 
-  const set = useCallback((newPresent: T) => {
-    setState((currentState) => {
-      const { past, present } = currentState;
-      if (newPresent === present) return currentState;
+    case SET: {
+      if (newPresent === present) return state;
       return {
         past: [...past, present],
         present: newPresent,
         future: [],
       };
-    });
-  }, []);
+    }
 
-  const reset = useCallback((newPresent: T) => {
-    setState(() => {
+    case RESET: {
       return {
         past: [],
         present: newPresent,
         future: [],
       };
-    });
-  }, []);
+    }
+  }
+};
 
+export const useUndo = <T>(initialPresent: T) => {
+  // const [state, setState] = useState<{
+  //   past: T[];
+  //   present: T;
+  //   future: T[];
+  // }>({
+  //   past: [],
+  //   present: initialPresent,
+  //   future: [],
+  // });
+
+  const [state, dispatch] = useReducer(undoReducer, {
+    past: [],
+    present: initialPresent,
+    future: [],
+  } as State<T>);
+
+  const canUndo = state.past.length !== 0;
+  const canRedo = state.future.length !== 0;
+
+  const undo = useCallback(() => dispatch({ type: UNDO }), []);
+  //   setState((currentState) => {
+  //     const { past, present, future } = currentState;
+  //     if (past.length === 0) return currentState;
+  //     const previous = past[past.length - 1];
+  //     const newPast = past.slice(0, past.length - 1);
+  //     return {
+  //       past: newPast,
+  //       present: previous,
+  //       future: [present, ...future],
+  //     };
+  //   });
+  const redo = useCallback(() => dispatch({ type: REDO }), []);
+  //   setState((currentState) => {
+  //     const { past, present, future } = currentState;
+  //     if (future.length === 0) return currentState;
+  //     const next = future[0];
+  //     const newFuture = future.slice(1);
+  //     return {
+  //       past: [...past, present],
+  //       present: next,
+  //       future: newFuture,
+  //     };
+  //   });
+  const set = useCallback(
+    (newPresent: T) => dispatch({ newPresent, type: SET }),
+    []
+  );
+  //   setState((currentState) => {
+  //     const { past, present } = currentState;
+  //     if (newPresent === present) return currentState;
+  //     return {
+  //       past: [...past, present],
+  //       present: newPresent,
+  //       future: [],
+  //     };
+  //   });
+  const reset = useCallback(
+    (newPresent: T) => dispatch({ newPresent, type: RESET }),
+    []
+  );
+  //   setState(() => {
+  //     return {
+  //       past: [],
+  //       present: newPresent,
+  //       future: [],
+  //     };
+  //   });
   return [state, { set, reset, undo, redo, canUndo, canRedo }] as const;
 };
